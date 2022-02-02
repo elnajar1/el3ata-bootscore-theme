535f662c52efd8b251db503db054a92d627787e0
Mahmoud El-Najar <31566070+moud449@users.noreply.github.com>
2022-02-02 21:10:19 +0200

    update page-classifier.php

--- page-classifier.php
+++ page-classifier.php
@@ -13,13 +13,13 @@
       
       $childs = get_terms( array(
         'taxonomy' => 'lecture_category',
-        'child_of'      => $parent->ID, 
+        'parent'      => $parent->ID, 
         'depth'  => 1,
         'hide_empty'  => false
       ));
       
       foreach( $childs as $child ):
-       // echo "- " . $child->name . "<hr>" ;
+        echo "- " . $child->name . "<hr>" ;
         //echo "<pre>"; var_dump($child); echo "</pre>"; 
       endforeach ;  
       

f8d83aee64883bd2b1db69c082e23b7cfc2d40ff
Mahmoud El-Najar <31566070+moud449@users.noreply.github.com>
2022-02-02 22:18:50 +0200

    update page-classifier.php

--- page-classifier.php
+++ page-classifier.php
@@ -23,6 +23,8 @@
         foreach( $level_one_clilds as $level_one_clild ):
             
             echo $level_one_clild->name . "<hr>" ;
+            
+            // #level_one_clild_posts#
             $level_one_clild_posts = get_posts(array(
                 'post_type' => 'lecture',
                 'numberposts' => -1,
@@ -30,19 +32,17 @@
                 array(
                   'taxonomy' => 'lecture_category',
                   'field' => $level_one_clild->term_id, 
-                  'terms' => $level_one_clild->term_id, /// Where term_id of Term 1 is "1".
                   'include_children' => false
                 )
                )
               ));
               
-              foreach ( $level_one_clild_posts as $post ) : setup_postdata( $post ); ?>
-              <li>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-              </li>
-              <?php endforeach; // Term Post foreach ?>
-              </ul>
-              <?php wp_reset_postdata(); 
+              echo "<ul>";
+              foreach ( $level_one_clild_posts as $post ) : setup_postdata( $post ); 
+                echo "<li><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>";
+              endforeach;  
+              echo "</ul>";
+              wp_reset_postdata(); // #level_one_clild_posts#
               
             //--level_tow_clilds--
             $level_tow_clilds = get_terms( array(
@@ -52,9 +52,30 @@
               'hide_empty'  => false
             ));
             foreach( $level_tow_clilds as $level_tow_clild ):
-              echo "- " . $level_tow_clild->name . "<hr>" ;
               
-               //---level_three_clild ---
+              echo "- " . $level_tow_clild->name . "<hr>" ;
+                
+                // ##level_one_clild_posts##
+                $level_tow_clild_posts = get_posts(array(
+                    'post_type' => 'lecture',
+                    'numberposts' => -1,
+                    'tax_query' => array(
+                    array(
+                      'taxonomy' => 'lecture_category',
+                      'field' => $level_two_clild->term_id, 
+                      'include_children' => false
+                    )
+                   )
+                  ));
+                  
+                  echo "<ul>";
+                  foreach ( $level_two_clild_posts as $post ) : setup_postdata( $post ); 
+                    echo "<li><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>";
+                  endforeach;  
+                  echo "</ul>";
+                  wp_reset_postdata(); // ##level_tow_clild_posts##
+                 
+               //---level_three_clilds ---
                $level_three_clilds = get_terms( array(
                 'taxonomy'    => 'lecture_category',
                 'parent'      => $level_tow_clild->term_id, 
@@ -62,12 +83,34 @@
                 'hide_empty'  => false
               ));
               foreach( $level_three_clilds as $level_three_clild ):
+                
                 echo "-- " . $level_three_clild->name . "<hr>" ;
-              endforeach ; //level_three_clild
+                
+                // ###level_three_clild_posts###
+                $level_three_clild_posts = get_posts(array(
+                    'post_type' => 'lecture',
+                    'numberposts' => -1,
+                    'tax_query' => array(
+                    array(
+                      'taxonomy' => 'lecture_category',
+                      'field' => $level_three_clild->term_id, 
+                      'include_children' => false
+                    )
+                   )
+                  ));
+                  
+                  echo "<ul>";
+                  foreach ( $level_three_clild_posts as $post ) : setup_postdata( $post ); 
+                    echo "<li><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>";
+                  endforeach;  
+                  echo "</ul>";
+                  wp_reset_postdata(); // ###level_one_clild_posts###
+                 
+              endforeach ; //-level_three_clild-
               
-            endforeach ;  //level_tow_clild
+            endforeach ;  //--level_tow_clild--
             
-        endforeach ; //level_one_clilds
+        endforeach ; //---level_one_clilds---
         ?>
       </div>
     </div>

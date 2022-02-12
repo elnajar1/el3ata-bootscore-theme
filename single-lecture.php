d2e67742c97e962a501e7fc78b97b7f01c42a80a
Mahmoud El-Najar <31566070+moud449@users.noreply.github.com>
2022-02-12 07:15:03 +0200

    update single-lecture.php

--- single-lecture.php
+++ single-lecture.php
@@ -65,7 +65,7 @@ echo $term->name . ">";
               ?> 
               
               <?php 
-                if ( !empty( the_post_thumbnail() ) ) { ?>
+                if ( !empty( has_post_thumbnail() ) ) { ?>
                   
                   <button  onclick ="document.getElementById('thumbnail').style.display = 'block' " class = "my-1 btn btn-sm btn-outline-secondary" >
                     <i class="text-warning bi bi-image"></i>

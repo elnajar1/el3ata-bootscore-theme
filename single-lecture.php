e81c8284ea2b325b34f6b542da7124f44efe56d6
Mahmoud El-Najar <31566070+moud449@users.noreply.github.com>
2022-02-12 06:31:59 +0200

    update single-lecture.php

--- single-lecture.php
+++ single-lecture.php
@@ -129,7 +129,7 @@
                   
                   ?>
                   <div class ="m-auto">
-                    <iframe class="text-center" src="http://www.youtube.com/embed/<?php echo $youtube_video_id[0]  ?>" title="<?php the_title(); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe class="m-auto" src="http://www.youtube.com/embed/<?php echo $youtube_video_id[0]  ?>" title="<?php the_title(); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                   </div>
                   <?php
                 }
